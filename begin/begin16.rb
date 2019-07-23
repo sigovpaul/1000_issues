1a22e0571459f6b6211870d42283e636e4067bf6
Sigov Paul <sigovpaul@gmail.com>
2019-07-23 13:31:10 +0300

    rename files

--- begin/begin16.rb
+++
@@ -1,6 +0,0 @@
-puts " Введите x1"
-x1 = gets.chomp.to_f
-puts " Введите x2"
-x2 = gets.chomp.to_f
-L = ( x2 - x1 ).abs
-puts "Расстояние между точками равно #{L}"
--- begin/begin22.rb
+++
@@ -1,9 +0,0 @@
-puts " Введите A"
-A = gets.chomp.to_f
-puts " Введите B"
-B = gets.chomp.to_f
-
-temp = B
-B = A
-A = temp
-puts A, B
