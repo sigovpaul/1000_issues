c87515ecd858569223c88400e10ecf88d983b7da
psigov <sigovpaul@gmail.com>
2019-07-24 13:58:42 +0300

    begin 17, vatiable's names changed

--- begin/begin_17.rb
+++ begin/begin_17.rb
@@ -1,13 +1,13 @@
-puts " Введите A"
-A = gets.chomp.to_f
-puts " Введите B"
-B = gets.chomp.to_f
-puts " Введите C"
-C = gets.chomp.to_f
+puts " Введите первую координату"
+first_coordinate = gets.chomp.to_f
+puts " Введите вторую координату"
+second_coordinate = gets.chomp.to_f
+puts " Введите третью координату"
+third_coordinate = gets.chomp.to_f
 
-AC = ( C - A ).abs
-BC = ( C - B ).abs
-ABC = AC + BC
+first_third_distance = ( third_coordinate - first_coordinate ).abs
+second_third_distance = ( third_coordinate - second_coordinate ).abs
+distance = first_third_distance + second_third_distance
 text = "Расстояние между точками"
-puts "#{text} A  и  C равно #{AC}.#{text} B и C равно #{BC}"
-puts "Сумма длин AC и BC равна #{ABC}"
+puts "#{text} A  и  C равно #{first_third_distance}.#{text} B и C равно #{second_third_distance}"
+puts "Сумма длин AC и BC равна #{distance}"
