1d1991286dd4a165afb3326c930e6748c5619eab
psigov <sigovpaul@gmail.com>
2019-08-05 17:30:04 +0300

    27 variavles names changed

--- begin/begin_27.rb
+++ begin/begin_27.rb
@@ -1,15 +1,16 @@
-puts " Введите A"
-A = gets.chomp.to_f
-=begin
-temp_A = A * A
-puts "A^2 равно #{temp_A}"
-temp_A = temp_A * temp_A
-puts "A^4 равно #{temp_A}"
-temp_A = temp_A * temp_A
-puts "A^8 равно #{temp_A}"
-=end
-n = Math.log(8, 2)
-for i in 1..n
-	A = A ** 2
-	puts A	
+puts "Введите число"
+input_variable = gets.chomp.to_f
+
+temp_variable = input_variable ** 2
+puts "#{input_variable} в степени 2 равно #{temp_variable}"
+temp_variable = temp_variable ** 2
+puts "#{input_variable} в 4й степени равно #{temp_variable}"
+temp_variable = temp_variable ** 2
+puts "#{input_variable} в 8й степени равно #{temp_variable}"
+
+
+border_number = Math.log(8, 2)
+for i in 1..border_number
+	input_variable = input_variable ** 2
+	puts input_variable	
 end
